ac0ae1975b63c2fbb0d700e6bfc9ca7a9bd925cf
Hunter Senft-Grupp <huntcsg@gmail.com>
2016-03-19 17:12:20 -0400

    __main__.py doesn't need to worry about printing usage. Do that elsewahere

--- bragly/__main__.py
+++ bragly/__main__.py
@@ -5,13 +5,17 @@ import sys
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
-    args = parse_args(args)
+    parser, args = parse_args(args)
+    if not args:
+        parser.print_help()
+        return 2
+    
     func = args.pop('func', None)
     if func is None:
         print("Operation failed for unexpected reason")
-        return -1
+        return 1
     result = func(**args)
     print(result)
     return 0
-
-main()
+if __name__ == '__main__':
+    main()
