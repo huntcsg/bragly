37ae39a9b1d0abed97340570775b2cba9c8be2e2
Hunter Senft-Grupp <hsenftgrupp@athenahealth.com>
2016-03-20 19:47:18 -0400

    Added comments to __main__.py

--- bragly/__main__.py
+++ bragly/__main__.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Entry point(s) for the command line interface"""
 from bragly.cli import parse_args
 import sys
 
+
 def main(args=None):
+    """Either takes arguments or gets them from sys.argv. Calls the bragly.cli
+        parsing function parse_args, and then executes the returned function.
+        See the usage at the command line by passing --help as an option.
+    Args:
+        args (list): A list of arguments that would be in sys.argv. If None,
+            args will be populated from sys.argv.
+
+    Returns:
+        int:
+            0 - If the program runs without exception.
+            1 - If the program runs but there is an unexpected exception
+            2 - If the program runs and no arguments are parsed form the command
+                line.
+    """
     if args is None:
         args = sys.argv[1:]
     parser, args = parse_args(args)
     if not args:
         parser.print_help()
         return 2
-    
+
     func = args.pop('func', None)
     if func is None:
         print("Operation failed for unexpected reason")
@@ -18,5 +35,7 @@ def main(args=None):
     for result in results:
         print(result, flush=True)
     return 0
+
+
 if __name__ == '__main__':
     main()
