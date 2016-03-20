272fd129c4c8fc01a5cfe7ab8a6ad2d0da46f24b
Hunter Senft-Grupp <hsenftgrupp@athenahealth.com>
2016-03-20 16:32:46 -0400

    updated config.py to read a user config file

--- bragly/config.py
+++ bragly/config.py
@@ -1,16 +1,48 @@
+import configparser
 import os
+
+BRAG_DIR = os.environ.get('BRAG_DIR', os.path.expanduser('~/.brag'))
+CONFIG_FILE_PATH = os.environ.get('BRAG_CONFIG_PATH', os.path.join(BRAG_DIR, 'config.ini'))
+
 def get_config(mechanism=None):
-    if mechanism is None:
-        mechanism = 'files'
 
     default_config = {
         'files': {
-            'file_dir': os.path.expanduser('~/.brag'),
+            'file_dir': BRAG_DIR,
             'form': 'log',
         },
     }
 
-    config = default_config[mechanism] # add logic to check a user ccnfiguration file
-    config['mechanism'] = mechanism
+    conf_parser = configparser.ConfigParser()
+    try:
+        with open(CONFIG_FILE_PATH, 'r') as f:
+            conf_parser.read_file(f)
+    except IOError:
+        print("No configuration found, using default configuration: ", end='')
 
-    return config
+    if mechanism is None:
+        if 'mechanism' in conf_parser:
+            mechanisms = [
+                mech for mech, onoff
+                in conf_parser['mechanism'].items()
+                if onoff.lower() == 'on'
+            ]
+            if len(mechanisms) > 1:
+                raise RuntimeError('Multiple mechanisms are on. Please turn all'
+                                   ' but one off.')
+        else:
+            mechanisms = ['files']
+    else:
+        mechanisms = [mechanism]
+
+    config = {}
+    for mechanism in mechanisms:
+        if mechanism in conf_parser:
+            overide_config = dict(conf_parser[mechanism])
+        else:
+            overide_config = {}
+
+        config[mechanism] = dict(default_config.get(mechanism, {}), **overide_config)
+
+    config[mechanism]['mechanism'] = mechanism
+    return config[mechanism]
