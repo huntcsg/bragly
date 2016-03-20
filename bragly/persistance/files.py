ef65a5bd42c46bb76d0b00772574962717eede3f
Hunter Senft-Grupp <hsenftgrupp@athenahealth.com>
2016-03-20 02:41:04 -0400

    file persistance documentation

--- bragly/persistance/files.py
+++ bragly/persistance/files.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+"""A module for writing, reading, and searching files for persistance of brags.
+    Implements the three public methods necessary for the bragly.persist API:
+        write : saves a message.
+        read : reads all messages in the relevant timeframe.
+        search: searches all messages according to a number of criteria
+
+Attributes:
+    MESSAGE_STR_TEMPLATE (str): A template string for the 'log' format.
+
+"""
+
+from __future__ import absolute_import, print_function
 import os
 import json
 import re
@@ -7,6 +20,18 @@ import arrow
 MESSAGE_STR_TEMPLATE = "[{timestamp}][{tags}] {message}\n"
 
 def write(message, file_path=None, file_dir=None, form='json'):
+    """Writes a message to a file in the given format.
+
+    Args:
+        message (dict): A message, it's timestamp, and any associated tags.
+        form (str): A message format, one of log, json, json-pretty
+        file_path (str): The file path for the source file. If None, the file_path will be generated from the file_dir
+            and the form
+        file_dir (str): The file directory for the source file. If None, the file_path will be used
+
+    Returns:
+        str: On success returns the word "success"
+    """
     if form == 'json':
         message_str = json.dumps(message)
     if form == 'json-pretty':
@@ -16,14 +41,26 @@ def write(message, file_path=None, file_dir=None, form='json'):
         timestamp = message['timestamp'].isoformat()
         message_str = MESSAGE_STR_TEMPLATE.format(timestamp=timestamp, tags=tags, message=message['message'])
 
-    file_path = get_file_path(form, file_path, file_dir)
+    file_path = _get_file_path(form, file_path, file_dir)
 
     with open(file_path, 'a') as f:
         f.write(message_str)
     
     return "success"
 
-def get_file_path(form, file_path=None, file_dir=None):
+def _get_file_path(form, file_path=None, file_dir=None):
+    """Given a format, and either a filepath or file directory, returns the proper filepath.
+        The format of the file, if dynamically generated from the file_dir will be:
+            /path/to/file/dir/brag-{format}.dat
+    Args:
+        form (str): A message format, one of log, json, json-pretty
+        file_path (str): The file path for the source file. If None, the file_path will be generated from the file_dir
+            and the form
+        file_dir (str): The file directory for the source file. If None, the file_path will be used
+
+    Returns:
+        str: A file path for the source file
+    """
     if file_path is None and file_dir is None:
         raise RuntimeError('No file_path or file_dir indicated.')
 
@@ -34,22 +71,44 @@ def get_file_path(form, file_path=None, file_dir=None):
 
 
 def read(start, end, out_form, form, file_dir=None, file_path=None):
-    file_path = get_file_path(form, file_path, file_dir)
+    """Reads out entries within the given time frame.
+
+    Args:
+        start (arrow.Arrow): The start date time to look for messages
+        end (arrow.Arrow): The end date time to look for messages
+        out_form (str): The output format. One of log, json, json-pretty
+        form (str): The format that the source file is in
+        file_dir (str): The file directory for the source file. If None, the file_path will be used.
+        file_path (str): The file path for the source file. If None, the file_path will be generated from the file_dir
+            and the form.
+    Yields:
+        str: A message line, in the requested format
+    """
+    file_path = _get_file_path(form, file_path, file_dir)
 
     if form == 'json-pretty':
         raise NotImplementedError('json-pretty format is not yet supported')
     with open(file_path, 'rb') as f:
         for line in f:
             line = line.decode('utf-8').strip()
-            parsed_line = parse_line(line, form=form)
+            parsed_line = _parse_line(line, form=form)
             if parsed_line.timestamp >= start and parsed_line.timestamp <= end:
                 if out_form != form:
-                    yield coerce_line(parsed_line, out_form)
+                    yield _coerce_line(parsed_line, out_form)
                 else:
                     yield line
 
 ParsedLine = namedtuple('ParsedLine', ['timestamp', 'tags', 'message'])
-def parse_line(line, form):
+def _parse_line(line, form):
+    """Parse a message line according to the form that is passed in, returning a ParsedLine object.
+
+    Args:
+        line (str): A line from the data file
+        form (str): Indicates the form that the line is in, one of log or json (json-pretty not supported)
+
+    Returns:
+        ParsedLine: The message in a convenient namedtuple
+    """
     if form == 'log':
         line_regex = re.compile(r'\[(.*)\]\[(.*)] (.*)')
         timestamp, tags, message = line_regex.findall(line)[0]
@@ -68,7 +127,17 @@ def parse_line(line, form):
     elif form == 'json-pretty':
         raise RuntimeError('No!')
 
-def coerce_line(parsed_line, out_form):
+def _coerce_line(parsed_line, out_form):
+    """ Coerce's a parsed line (named tuple) into the requested output format.
+
+    Args:
+        parsed_line (ParsedLine): A named tuple of the message.
+        out_form (str): The output format, one of log, json, json-pretty
+
+    Returns:
+        str: The line in the format requested
+        ParsedLine: Passthrough of the parsed_line arg, if requested
+    """
     if out_form == 'parsed_line':
         return parsed_line
     timestamp = parsed_line.timestamp.isoformat()
@@ -86,15 +155,32 @@ def coerce_line(parsed_line, out_form):
 
 
 def search(start, end, out_form, tags, text, all_args, form, file_dir=None, file_path=None):
+    """Given a file path or a file directory and form, searches the files according to the search parameters.
+
+    Args:
+        start (arrow.Arrow): The start date time to look for messages
+        end (arrow.Arrow): The end date time to look for messages
+        out_form (str): The output format. One of log, json, json-pretty
+        tags (list): A list of tags to look for
+        text (list): A list of text tokens (words) to look for
+        all_args (bool): Indicates whether all tags and text must exist for the message to be surfaces
+        form (str): The format that the source file is in
+        file_dir (str): The file directory for the source file. If None, the file_path will be used.
+        file_path (str): The file path for the source file. If None, the file_path will be generated from the file_dir
+            and the form.
+
+    Yields:
+        str: A line of text representing one result of the search
+    """
     base_results = read(start, end, 'parsed_line', form, file_dir, file_path)
     for result in base_results:
         if not all_args:
             if tags and set(tags).intersection(set(result.tags)):
-                yield coerce_line(result, out_form)
+                yield _coerce_line(result, out_form)
             elif text and set(text).intersection((set(result.message.split(' ')))):
-                yield coerce_line(result, out_form)
+                yield _coerce_line(result, out_form)
             elif not text and not tags:
-                yield coerce_line(result, out_form)
+                yield _coerce_line(result, out_form)
         else:
             tags_in_tags = False
             text_in_message = False
@@ -107,4 +193,4 @@ def search(start, end, out_form, tags, text, all_args, form, file_dir=None, file
             elif not text:
                 text_in_message = True
             if tags_in_tags and text_in_message:
-                yield coerce_line(result, out_form)
+                yield _coerce_line(result, out_form)
