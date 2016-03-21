d1dffd9601ad3ac80f67dd25c3096167de8b1be2
Hunter Senft-Grupp <hsenftgrupp@athenahealth.com>
2016-03-20 20:14:24 -0400

    bugfix for python 2.7 and documentation

--- bragly/config.py
+++ bragly/config.py
@@ -1,10 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Loads, parses and returns values related to configuration
+
+    Attributes:
+        BRAG_DIR (str): The directory to use when reading and writing data.
+            defaults to '~/.brag' (~/ is the user directory, and will be the
+             correct value based on the platform. It can be overridden by the
+             environmental variable BRAG_DIR.
+        CONFIG_FILE_PATH (str): The file path for the configuration file.
+            Defaults to $BRAG_DIR/config.ini, but can be overridden by setting
+            the environmental variable BRAG_CONFIG_PATH.
+"""
+from __future__ import print_function, absolute_import
 import configparser
 import os
 
 BRAG_DIR = os.environ.get('BRAG_DIR', os.path.expanduser('~/.brag'))
 CONFIG_FILE_PATH = os.environ.get('BRAG_CONFIG_PATH', os.path.join(BRAG_DIR, 'config.ini'))
 
+
 def get_config(mechanism=None):
+    """Gets the configuration from a file, if possible. If not, uses the
+        default.
+
+    Args:
+        mechanism (str): The mechanism to use for persistance. One of 'files',
+            'reldb' (relational database), or 'mongdb'.
+
+    Returns:
+        dict: A dictionary of configuration values relevant to the given
+            persistence mechanism.
+    """
 
     default_config = {
         'files': {
@@ -42,7 +67,12 @@ def get_config(mechanism=None):
         else:
             overide_config = {}
 
-        config[mechanism] = dict(default_config.get(mechanism, {}), **overide_config)
+        # Sets the configuration as the default configuration, and then
+        # overrides from the configuration file.
+        config[mechanism] = dict(
+            default_config.get(mechanism, {}),
+            **overide_config
+        )
 
     config[mechanism]['mechanism'] = mechanism
     return config[mechanism]
