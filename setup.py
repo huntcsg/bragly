9835058e5c66045fd4f8a36374a52b0b8011a666
Hunter Senft-Grupp <huntcsg@gmail.com>
2016-03-22 01:09:25 -0400

    Updated setup.py version number and .travis deploy options

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='bragly',
-    version='0.1.2',
+    version='0.1.3',
     description='A small tool to remind yourself of your day-to-day accomplishments',
     long_description = readme,
     url='http://github.com/huntcsg/bragly',
